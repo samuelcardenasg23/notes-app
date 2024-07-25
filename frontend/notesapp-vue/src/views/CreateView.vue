ca22294ad533cb000c3c1365950b2cd4f2fd662e
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-07-25 17:25:33 -0500

    Add categories when creating notes

--- frontend/notesapp-vue/src/views/CreateView.vue
+++ frontend/notesapp-vue/src/views/CreateView.vue
@@ -18,6 +18,16 @@
                 placeholder="Content"
                 required
             />
+            <label for="categories" class="mt-5 block mb-2 text-md font-medium text-gray-900 dark:text-white">Select Categories</label>
+            <select 
+                class="p-3.5 rounded border-b border-gray-300 text-gray-900 outline-none" 
+                v-model="selectedCategories" 
+                multiple
+            >
+                <option v-for="category in categories" :key="category.id" :value="category.id">
+                    {{ category.name }}
+                </option>
+            </select>
             <button class="bg-blue-500 hover:bg-blue-600 mt-5 py-2.5 rounded">Add Note</button>
         </form>
   </div>
@@ -25,14 +35,20 @@
 
 <script>
     import { show_alert, sendRequest } from '@/functions';
+    import axios from 'axios';
 
     export default{
         data(){
             return {
                 title: '',
                 content: '',
+                categories: [],
+                selectedCategories: [],
                 url: 'http://127.0.0.1:8000/api/notes',
             }
+        },
+        mounted() {
+            this.getCategories()
         },
         methods: {
             store() {
@@ -44,9 +60,17 @@
                     const parameters = {
                         title: this.title.trim(),
                         content: this.content.trim(),
+                        category_ids: this.selectedCategories,
                     }
                     sendRequest('POST', parameters, this.url, 'Note saved!')
                 }
+            },
+            getCategories() {
+                axios.get('http://127.0.0.1:8000/api/categories').then(
+                    response => {
+                        this.categories = response.data.data
+                    }
+                    ).catch(err => console.log(err.message));
             }
         }
     }
